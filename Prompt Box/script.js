efa3a73ac11975c97ed7242e3ed121b1fd1b95df
Vatsal Patel <158587779+vatsal28sept@users.noreply.github.com>
2024-04-17 10:25:57 +0530

    Add files via upload

---
+++ Prompt Box/script.js
@@ -0,0 +1,10 @@
+function ShowPrompt(){
+    var a = prompt("Enter Value Of N1 :" );
+    var b = prompt("enter Value Of N2 : ");
+
+    var c = parseInt(a) + parseInt(b);
+
+    document.write("Value Of N1 Is : " ,a + "<br>");
+    document.write("Value Of N2 Is : ",b + "<br>");
+    document.write("the Value Of C Is : ",c);
+}
