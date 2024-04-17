56edd88600bd6dc8acd2704f6b559c42b80b8000
Vatsal Patel <158587779+vatsal28sept@users.noreply.github.com>
2024-04-17 10:25:43 +0530

    Add files via upload

---
+++ Confirm Box/script.js
@@ -0,0 +1,9 @@
+function ShowPopup(){
+    var a = confirm("Thank You For Clicking Button");
+    if(a == true){
+        document.write("Ok Button clicked");
+    }
+    else{
+        document.write("Cencle Button clicked");
+    }
+}
