65e8d0206c2dd9b102b48922c562d06422d97d17
Vatsal Patel <158587779+vatsal28sept@users.noreply.github.com>
2024-04-18 09:52:01 +0530

    Add files via upload

---
+++ Mouse Events/script.js
@@ -0,0 +1,26 @@
+function myfnc() {
+  alert("Hello Vatsal Here!!");
+}
+function bigimg(x) {
+  x.style.height = "100px";
+  x.style.width = "100px";
+}
+function smallimg(x) {
+  x.style.height = "50px";
+  x.style.width = "50px";
+}
+function big(x) {
+  x.style.backgroundColor = "red";
+  x.style.fontSize = "100px";
+  x.innerHTML = "Release Me";
+}
+function small(x) {
+  x.style.backgroundColor = "yellow";
+  x.style.fontSize = "50px";
+  x.innerHTML = "click me";
+}
+function move(x){
+    x.style.backgroundColor = "red";
+    x.style.fontSize = "100px";
+    x.innerHTML = "Patel"
+}
